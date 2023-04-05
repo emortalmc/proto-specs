8f584445c7d78bb480744ee82c391cfacef710d0
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-04-05 19:41:25 +0100

    Rename gRPC converters

--- src/main/java/dev/emortal/api/utils/ProtoDurationConverter.java
+++ src/main/java/dev/emortal/api/utils/ProtoDurationConverter.java
@@ -1,24 +1,29 @@
 package dev.emortal.api.utils;
 
 import com.google.protobuf.Duration;
+import org.jetbrains.annotations.NotNull;
 
-public class GrpcDurationConverter {
+public class ProtoDurationConverter {
 
-    public static Duration convert(java.time.Duration duration) {
+    public static @NotNull Duration toProto(@NotNull java.time.Duration duration) {
         return Duration.newBuilder()
                 .setSeconds(duration.getSeconds())
                 .setNanos(duration.getNano())
                 .build();
     }
 
-    public static Duration convertMillis(long millis) {
+    public static @NotNull java.time.Duration fromProto(@NotNull Duration duration) {
+        return java.time.Duration.ofSeconds(duration.getSeconds(), duration.getNanos());
+    }
+
+    public static @NotNull Duration toProtoFromMillis(long millis) {
         return Duration.newBuilder()
                 .setSeconds(millis / 1000)
                 .setNanos((int) ((millis % 1000) * 1000000))
                 .build();
     }
 
-    public static Duration convertNanos(long nanos) {
+    public static @NotNull Duration toProtoFromNanos(long nanos) {
         return Duration.newBuilder()
                 .setSeconds(nanos / 1000000000)
                 .setNanos((int) (nanos % 1000000000))
--- src/main/java/dev/emortal/api/utils/ProtoTimestampConverter.java
+++ src/main/java/dev/emortal/api/utils/ProtoTimestampConverter.java
@@ -1,34 +1,38 @@
 package dev.emortal.api.utils;
 
 import com.google.protobuf.Timestamp;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.Instant;
 
-public class GrpcTimestampConverter {
+public class ProtoTimestampConverter {
 
-    public static Timestamp convert(Instant instant) {
+    public static @NotNull Timestamp toProto(@NotNull Instant instant) {
         return Timestamp.newBuilder()
                 .setSeconds(instant.getEpochSecond())
                 .setNanos(instant.getNano())
                 .build();
     }
 
-    public static Instant reverse(Timestamp timestamp) {
+    public static @NotNull Instant fromProto(@NotNull Timestamp timestamp) {
         return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
     }
 
-    public static Timestamp now() {
-        return convert(Instant.now());
+    public static @NotNull Timestamp now() {
+        return toProto(Instant.now());
     }
 
-    public static Timestamp convertMillis(long millis) {
+    public static @NotNull Timestamp toProtoFromMillis(long millis) {
+        if (millis < 0) throw new IllegalArgumentException("Millis cannot be negative");
         return Timestamp.newBuilder()
                 .setSeconds(millis / 1000)
                 .setNanos((int) ((millis % 1000) * 1000000))
                 .build();
     }
 
-    public static Timestamp convertNanos(long nanos) {
+    public static @NotNull Timestamp toProtoFromNanos(long nanos) {
+        if (nanos < 0) throw new IllegalArgumentException("Nanos cannot be negative");
+
         return Timestamp.newBuilder()
                 .setSeconds(nanos / 1000000000)
                 .setNanos((int) (nanos % 1000000000))
