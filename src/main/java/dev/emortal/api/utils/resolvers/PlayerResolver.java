16631c0d5d49c269c237bdc53afaaedaee5524f0
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-08-21 10:16:16 +0100

    Added resolving by UUID to player resolver

--- src/main/java/dev/emortal/api/utils/resolvers/PlayerResolver.java
+++ src/main/java/dev/emortal/api/utils/resolvers/PlayerResolver.java
@@ -2,17 +2,14 @@ package dev.emortal.api.utils.resolvers;
 
 import dev.emortal.api.model.mcplayer.McPlayer;
 import dev.emortal.api.service.mcplayer.McPlayerService;
-import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.StatusRuntimeException;
 import org.jetbrains.annotations.Blocking;
-import org.jetbrains.annotations.NonBlocking;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
 import java.util.UUID;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -22,62 +19,58 @@ import java.util.function.Function;
 public final class PlayerResolver {
 
     private final McPlayerService playerService;
+    private final Function<UUID, CachedMcPlayer> platformResolverById;
     // The platform resolver is used to resolve a player's data from a local source to avoid calling the service if they're on the same server.
-    private final Function<String, CachedMcPlayer> platformResolver;
+    private final Function<String, CachedMcPlayer> platformResolverByName;
 
-    public PlayerResolver(@NotNull McPlayerService playerService, @NotNull Function<String, CachedMcPlayer> platformResolver) {
+    public PlayerResolver(@NotNull McPlayerService playerService, @NotNull Function<UUID, CachedMcPlayer> platformResolverById,
+                          @NotNull Function<String, CachedMcPlayer> platformResolverByName) {
         this.playerService = playerService;
-        this.platformResolver = platformResolver;
+        this.platformResolverById = platformResolverById;
+        this.platformResolverByName = platformResolverByName;
     }
 
-    /**
-     * This will first check the platform resolver, and if it returns null, it will check the mc player service.
-     */
+    @Blocking
+    public @Nullable CachedMcPlayer getPlayerData(@NotNull UUID uuid) throws StatusException {
+        CachedMcPlayer platformPlayer = this.platformResolverById.apply(uuid);
+        if (platformPlayer != null) return platformPlayer;
+
+        return this.requestMcPlayer(uuid);
+    }
+
     @Blocking
     public @Nullable CachedMcPlayer getPlayerData(@NotNull String username) throws StatusException {
         String usernameLowercase = username.toLowerCase(Locale.ROOT);
 
-        CachedMcPlayer alternativeOption = this.platformResolver.apply(usernameLowercase);
-        if (alternativeOption != null) return alternativeOption;
+        CachedMcPlayer platformPlayer = this.platformResolverByName.apply(usernameLowercase);
+        if (platformPlayer != null) return platformPlayer;
 
-        try {
-            return this.requestMcPlayer(usernameLowercase);
-        } catch (StatusRuntimeException exception) {
-            throw new StatusException(exception.getStatus(), exception.getTrailers());
-        }
-    }
-
-    /**
-     * This will first check the platform resolver, and if it returns null, it will check the mc player service.
-     *
-     * <p>
-     * This method is asynchronous, and will offload the request to the mc player service to a virtual thread.
-     */
-    @NonBlocking
-    public void retrievePlayerData(@NotNull String username, @NotNull Consumer<CachedMcPlayer> callback, @NotNull Consumer<Status> errorCallback) {
-        String usernameLowercase = username.toLowerCase();
-
-        CachedMcPlayer alternativeOption = this.platformResolver.apply(usernameLowercase);
-        if (alternativeOption != null) {
-            callback.accept(alternativeOption);
-            return;
-        }
-
-        Thread.startVirtualThread(() -> {
-            try {
-                CachedMcPlayer player = this.requestMcPlayer(usernameLowercase);
-                callback.accept(player);
-            } catch (StatusRuntimeException exception) {
-                errorCallback.accept(exception.getStatus());
-            }
-        });
+        return this.requestMcPlayer(usernameLowercase);
     }
 
     @Blocking
-    private @Nullable CachedMcPlayer requestMcPlayer(@NotNull String username) throws StatusRuntimeException {
-        McPlayer player = this.playerService.getPlayerByUsername(username);
-        if (player == null) return null;
+    private @Nullable CachedMcPlayer requestMcPlayer(@NotNull UUID uuid) throws StatusException {
+        McPlayer player;
+        try {
+            player = this.playerService.getPlayerById(uuid);
+        } catch (StatusRuntimeException exception) {
+            throw new StatusException(exception.getStatus(), exception.getTrailers());
+        }
 
+        if (player == null) return null;
+        return new CachedMcPlayer(UUID.fromString(player.getId()), player.getCurrentUsername(), player.hasCurrentServer());
+    }
+
+    @Blocking
+    private @Nullable CachedMcPlayer requestMcPlayer(@NotNull String username) throws StatusException {
+        McPlayer player;
+        try {
+            player = this.playerService.getPlayerByUsername(username);
+        } catch (StatusRuntimeException exception) {
+            throw new StatusException(exception.getStatus(), exception.getTrailers());
+        }
+
+        if (player == null) return null;
         return new CachedMcPlayer(UUID.fromString(player.getId()), player.getCurrentUsername(), player.hasCurrentServer());
     }
 
