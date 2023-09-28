ca3c8ed246bb154236792f33f346565b165d1050
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-09-28 11:47:54 +0100

    Add javax.annotations-api to remove errors about @Generated not existing

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
     compileOnly("org.jetbrains:annotations:24.0.1")
     annotationProcessor("org.jetbrains:annotations:24.0.1")
 
+    // Protobuf uses @javax.annotation.Generated on generated types, and sometimes this isn't available.
+    // This make sure it's always available
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
     api("io.grpc:grpc-netty:$grpcVersion")
