ad444ef6ef20e3a363be64c5ab2631dfc443e67d
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2024-03-04 21:45:40 +0000

    feat(party): broadcast message

--- gen/go/message/party/messages.pb.go
+++ gen/go/message/party/messages.pb.go
@@ -550,6 +550,53 @@ func (x *PartySettingsChangedMessage) GetSettings() *party.PartySettings {
 	return nil
 }
 
+type PartyBroadcastMessage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Party *party.Party `protobuf:"bytes,1,opt,name=party,proto3" json:"party,omitempty"`
+}
+
+func (x *PartyBroadcastMessage) Reset() {
+	*x = PartyBroadcastMessage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_party_messages_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PartyBroadcastMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PartyBroadcastMessage) ProtoMessage() {}
+
+func (x *PartyBroadcastMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_party_messages_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PartyBroadcastMessage.ProtoReflect.Descriptor instead.
+func (*PartyBroadcastMessage) Descriptor() ([]byte, []int) {
+	return file_party_messages_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *PartyBroadcastMessage) GetParty() *party.Party {
+	if x != nil {
+		return x.Party
+	}
+	return nil
+}
+
 var File_party_messages_proto protoreflect.FileDescriptor
 
 var file_party_messages_proto_rawDesc = []byte{
@@ -618,13 +665,18 @@ var file_party_messages_proto_rawDesc = []byte{
 	0x67, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x22, 0x2e, 0x65, 0x6d, 0x6f, 0x72, 0x74,
 	0x61, 0x6c, 0x2e, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2e, 0x70, 0x61, 0x72, 0x74, 0x79, 0x2e, 0x50,
 	0x61, 0x72, 0x74, 0x79, 0x53, 0x65, 0x74, 0x74, 0x69, 0x6e, 0x67, 0x73, 0x52, 0x08, 0x73, 0x65,
-	0x74, 0x74, 0x69, 0x6e, 0x67, 0x73, 0x42, 0x58, 0x0a, 0x1d, 0x64, 0x65, 0x76, 0x2e, 0x65, 0x6d,
-	0x6f, 0x72, 0x74, 0x61, 0x6c, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67,
-	0x65, 0x2e, 0x70, 0x61, 0x72, 0x74, 0x79, 0x50, 0x01, 0x5a, 0x35, 0x67, 0x69, 0x74, 0x68, 0x75,
-	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x65, 0x6d, 0x6f, 0x72, 0x74, 0x61, 0x6c, 0x6d, 0x63, 0x2f,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2d, 0x73, 0x70, 0x65, 0x63, 0x73, 0x2f, 0x67, 0x65, 0x6e, 0x2f,
-	0x67, 0x6f, 0x2f, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2f, 0x70, 0x61, 0x72, 0x74, 0x79,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x74, 0x69, 0x6e, 0x67, 0x73, 0x22, 0x49, 0x0a, 0x15, 0x50, 0x61, 0x72, 0x74, 0x79, 0x42,
+	0x72, 0x6f, 0x61, 0x64, 0x63, 0x61, 0x73, 0x74, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12,
+	0x30, 0x0a, 0x05, 0x70, 0x61, 0x72, 0x74, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a,
+	0x2e, 0x65, 0x6d, 0x6f, 0x72, 0x74, 0x61, 0x6c, 0x2e, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2e, 0x70,
+	0x61, 0x72, 0x74, 0x79, 0x2e, 0x50, 0x61, 0x72, 0x74, 0x79, 0x52, 0x05, 0x70, 0x61, 0x72, 0x74,
+	0x79, 0x42, 0x58, 0x0a, 0x1d, 0x64, 0x65, 0x76, 0x2e, 0x65, 0x6d, 0x6f, 0x72, 0x74, 0x61, 0x6c,
+	0x2e, 0x61, 0x70, 0x69, 0x2e, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2e, 0x70, 0x61, 0x72,
+	0x74, 0x79, 0x50, 0x01, 0x5a, 0x35, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x65, 0x6d, 0x6f, 0x72, 0x74, 0x61, 0x6c, 0x6d, 0x63, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x2d, 0x73, 0x70, 0x65, 0x63, 0x73, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x6d, 0x65,
+	0x73, 0x73, 0x61, 0x67, 0x65, 0x2f, 0x70, 0x61, 0x72, 0x74, 0x79, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -639,7 +691,7 @@ func file_party_messages_proto_rawDescGZIP() []byte {
 	return file_party_messages_proto_rawDescData
 }
 
-var file_party_messages_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_party_messages_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
 var file_party_messages_proto_goTypes = []interface{}{
 	(*PartyCreatedMessage)(nil),         // 0: emortal.message.party.PartyCreatedMessage
 	(*PartyDeletedMessage)(nil),         // 1: emortal.message.party.PartyDeletedMessage
@@ -651,26 +703,28 @@ var file_party_messages_proto_goTypes = []interface{}{
 	(*PartyClosedMessage)(nil),          // 7: emortal.message.party.PartyClosedMessage
 	(*PartyLeaderChangedMessage)(nil),   // 8: emortal.message.party.PartyLeaderChangedMessage
 	(*PartySettingsChangedMessage)(nil), // 9: emortal.message.party.PartySettingsChangedMessage
-	(*party.Party)(nil),                 // 10: emortal.model.party.Party
-	(*party.PartyInvite)(nil),           // 11: emortal.model.party.PartyInvite
-	(*party.PartyMember)(nil),           // 12: emortal.model.party.PartyMember
-	(*party.PartySettings)(nil),         // 13: emortal.model.party.PartySettings
+	(*PartyBroadcastMessage)(nil),       // 10: emortal.message.party.PartyBroadcastMessage
+	(*party.Party)(nil),                 // 11: emortal.model.party.Party
+	(*party.PartyInvite)(nil),           // 12: emortal.model.party.PartyInvite
+	(*party.PartyMember)(nil),           // 13: emortal.model.party.PartyMember
+	(*party.PartySettings)(nil),         // 14: emortal.model.party.PartySettings
 }
 var file_party_messages_proto_depIdxs = []int32{
-	10, // 0: emortal.message.party.PartyCreatedMessage.party:type_name -> emortal.model.party.Party
-	10, // 1: emortal.message.party.PartyDeletedMessage.party:type_name -> emortal.model.party.Party
-	10, // 2: emortal.message.party.PartyEmptiedMessage.party:type_name -> emortal.model.party.Party
-	11, // 3: emortal.message.party.PartyInviteCreatedMessage.invite:type_name -> emortal.model.party.PartyInvite
-	12, // 4: emortal.message.party.PartyPlayerJoinedMessage.member:type_name -> emortal.model.party.PartyMember
-	12, // 5: emortal.message.party.PartyPlayerLeftMessage.member:type_name -> emortal.model.party.PartyMember
-	12, // 6: emortal.message.party.PartyPlayerLeftMessage.kicked_by:type_name -> emortal.model.party.PartyMember
-	12, // 7: emortal.message.party.PartyLeaderChangedMessage.new_leader:type_name -> emortal.model.party.PartyMember
-	13, // 8: emortal.message.party.PartySettingsChangedMessage.settings:type_name -> emortal.model.party.PartySettings
-	9,  // [9:9] is the sub-list for method output_type
-	9,  // [9:9] is the sub-list for method input_type
-	9,  // [9:9] is the sub-list for extension type_name
-	9,  // [9:9] is the sub-list for extension extendee
-	0,  // [0:9] is the sub-list for field type_name
+	11, // 0: emortal.message.party.PartyCreatedMessage.party:type_name -> emortal.model.party.Party
+	11, // 1: emortal.message.party.PartyDeletedMessage.party:type_name -> emortal.model.party.Party
+	11, // 2: emortal.message.party.PartyEmptiedMessage.party:type_name -> emortal.model.party.Party
+	12, // 3: emortal.message.party.PartyInviteCreatedMessage.invite:type_name -> emortal.model.party.PartyInvite
+	13, // 4: emortal.message.party.PartyPlayerJoinedMessage.member:type_name -> emortal.model.party.PartyMember
+	13, // 5: emortal.message.party.PartyPlayerLeftMessage.member:type_name -> emortal.model.party.PartyMember
+	13, // 6: emortal.message.party.PartyPlayerLeftMessage.kicked_by:type_name -> emortal.model.party.PartyMember
+	13, // 7: emortal.message.party.PartyLeaderChangedMessage.new_leader:type_name -> emortal.model.party.PartyMember
+	14, // 8: emortal.message.party.PartySettingsChangedMessage.settings:type_name -> emortal.model.party.PartySettings
+	11, // 9: emortal.message.party.PartyBroadcastMessage.party:type_name -> emortal.model.party.Party
+	10, // [10:10] is the sub-list for method output_type
+	10, // [10:10] is the sub-list for method input_type
+	10, // [10:10] is the sub-list for extension type_name
+	10, // [10:10] is the sub-list for extension extendee
+	0,  // [0:10] is the sub-list for field type_name
 }
 
 func init() { file_party_messages_proto_init() }
@@ -799,6 +853,18 @@ func file_party_messages_proto_init() {
 				return nil
 			}
 		}
+		file_party_messages_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PartyBroadcastMessage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	file_party_messages_proto_msgTypes[5].OneofWrappers = []interface{}{}
 	type x struct{}
@@ -807,7 +873,7 @@ func file_party_messages_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_party_messages_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   10,
+			NumMessages:   11,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
